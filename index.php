1569490a8d1c9c7df77373a2d66d9e2eaa8bc99d
RRx03 <roman_roux@icloud.com>
2025-03-19 19:14:21 +0100

    update

--- index.php
+++ index.php
@@ -1,9 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>PHP Test</title>
+    <title>La Chouette</title>
+    <link rel="stylesheet" type="text/css" href="include/style/style.css">
 </head>
 <body>
-    <?php echo '<p>Hello World</p>'; ?>
+    <header>
+        <h1>La Chouette</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="index.php?page=about">A propos</a></li>
+            <li><a href="index.php?page=contact">Contact</a></li>
+        </ul>
+    </nav>
+    <main>
+        <?php
+        if (isset($_GET['page'])) {
+            $page = $_GET['page'];
+            if ($page == 'about') {
+                include('about.php');
+            } elseif ($page == 'contact') {
+                include('contact.php');
+            } else {
+                include('home.php');
+            }
+        } else {
+            include('home.php');
+        }
+        ?>
+    </main>
+    <footer>
+        <p>&copy; 2020 La Chouette</p>
+    </footer>
 </body>
 </html>
